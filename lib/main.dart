2fbb0d421817e5d35cc0fe43126a899d04ff7a45
saci zakaria <sacideveloppeur@gmail.com>
2021-12-16 21:04:29 +0100

    void main() / CounterScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
+import 'counter_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: null,
+    return  MaterialApp(
+      home: CounterScreen(),
     );
 
 
