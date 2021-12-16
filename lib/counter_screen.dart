11810a44e0072fc6c58eeb815142961c8535bd80
saci zakaria <sacideveloppeur@gmail.com>
2021-12-16 21:18:24 +0100

    int counter

--- lib/counter_screen.dart
+++ lib/counter_screen.dart
@@ -5,25 +5,28 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatelessWidget {
+  int counter = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter "),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(onPressed: () {}, child: Text("MINUS ")),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: Text(
-              "2",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(onPressed: () {}, child: Text("MINUS ")),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                "2",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
             ),
-          ),
-          TextButton(onPressed: () {}, child: Text("PLUS ")),
-        ],
+            TextButton(onPressed: () {}, child: Text("PLUS ")),
+          ],
+        ),
       ),
     );
   }
