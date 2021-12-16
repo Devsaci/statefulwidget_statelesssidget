fea66c48271ccb3d85d9eca529909994541f7949
saci zakaria <sacideveloppeur@gmail.com>
2021-12-16 20:42:11 +0100

    CounterScreen

---
+++ android/app/src/main/java/com/example/statefulwidget_statelesssidget/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.statefulwidget_statelesssidget;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+
+      ),
+    );
+  }
+}
