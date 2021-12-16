adbd43e478a40f740a25c558a1fa64cade88dcdd
saci zakaria <sacideveloppeur@gmail.com>
2021-12-16 21:11:51 +0100

    Text

--- lib/counter_screen.dart
+++ lib/counter_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatelessWidget {
@@ -12,7 +14,7 @@ class CounterScreen extends StatelessWidget {
       body: Row(
         children: [
           TextButton(onPressed: () {}, child: Text("MINUS ")),
-
+          Text("2",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
           TextButton(onPressed: () {}, child: Text("PLUS ")),
         ],
       ),
