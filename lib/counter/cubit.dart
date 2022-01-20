ad27bdf7c3a8e9d84fdce4ff4c2bcf04c330fdfd
saci zakaria <sacideveloppeur@gmail.com>
2022-01-20 15:31:21 +0100

    get(context)

--- lib/counter/cubit.dart
+++ lib/counter/cubit.dart
@@ -1,6 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:statefulwidget_statelesssidget/counter/states.dart';
 
 class CounterCubit extends Cubit<CounterStates> {
   CounterCubit() : super(CounterInitialState());
+
+ static CounterCubit get(context) => BlocProvider.of(context);
+
+
 }
