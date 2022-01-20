e828f2bf861e1eda03d7fbccd7b401f5c9a33595
saci zakaria <sacideveloppeur@gmail.com>
2022-01-20 17:59:38 +0100

    BlocOverrides

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:statefulwidget_statelesssidget/shared/bloc_observer.dart';
 
 import 'counter/counter_screen.dart';
 
 void main(){
+  BlocOverrides.runZoned(
+        () {
+      // Use cubits...
+    },
+    blocObserver: MyBlocObserver(),
+  );
   runApp(MyApp());
 }
 
