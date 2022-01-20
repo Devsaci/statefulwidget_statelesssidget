58c5b48e9b116071837efff73b2acbc1d3938677
saci zakaria <sacideveloppeur@gmail.com>
2022-01-20 12:08:24 +0100

    BlocProvider

--- lib/counter/counter_screen.dart
+++ lib/counter/counter_screen.dart
@@ -3,40 +3,45 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:statefulwidget_statelesssidget/counter/cubit.dart';
 
 class CounterScreen extends StatelessWidget {
   int counter = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Counter "),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: () {
-                  counter--;
-                  print(counter);
-                },
-                child: Text("MINUS ")),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0),
-              child: Text(
-                '$counter',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+    return BlocProvider(
+      create: (BuildContext context) => CounterCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Counter "),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    counter--;
+                    print(counter);
+                  },
+                  child: Text("MINUS ")),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Text(
+                  '$counter',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
               ),
-            ),
-            TextButton(
-                onPressed: () {
-                  counter++;
-                  print(counter);
-                },
-                child: Text("PLUS ")),
-          ],
+              TextButton(
+                  onPressed: () {
+                    counter++;
+                    print(counter);
+                  },
+                  child: Text("PLUS ")),
+            ],
+          ),
         ),
       ),
     );
