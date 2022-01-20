4c878d6628dc5f8a598df1dcd369bbd315b51e07
saci zakaria <sacideveloppeur@gmail.com>
2022-01-20 15:18:26 +0100

    BlocConsumer

--- lib/counter/counter_screen.dart
+++ lib/counter/counter_screen.dart
@@ -5,6 +5,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:statefulwidget_statelesssidget/counter/cubit.dart';
+import 'package:statefulwidget_statelesssidget/counter/states.dart';
 
 class CounterScreen extends StatelessWidget {
   int counter = 1;
@@ -13,38 +14,40 @@ class CounterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (BuildContext context) => CounterCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Counter "),
-        ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  onPressed: () {
-                    counter--;
-                    print(counter);
-                  },
-                  child: Text("MINUS ")),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: Text(
-                  '$counter',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      child: BlocConsumer<CounterCubit, CounterStates>(
+        listener: (BuildContext context, state) {},
+        builder: (BuildContext context, Object? state) => Scaffold(
+          appBar: AppBar(
+            title: Text("Counter "),
+          ),
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      counter--;
+                      print(counter);
+                    },
+                    child: Text("MINUS ")),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Text(
+                    '$counter',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
                 ),
-              ),
-              TextButton(
-                  onPressed: () {
-                    counter++;
-                    print(counter);
-                  },
-                  child: Text("PLUS ")),
-            ],
+                TextButton(
+                    onPressed: () {
+                      counter++;
+                      print(counter);
+                    },
+                    child: Text("PLUS ")),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
